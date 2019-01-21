bfb37ac5c4f0ddee8a6f07b23d74b003a6af38ce
elzed <elzed@users.noreply.github.com>
2019-01-21 15:14:03 -0800

    Get accounts from web3 ether module and add test

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -9,3 +9,20 @@ const Web3 = require('web3');
 // Create an instance of Web3
 // and tell it to connect to local test network
 const web3 = new Web3(ganache.provider());
+
+beforeEach(() => {
+    // Get a list of all accounts
+    // Use the eth module from web3
+    web3.eth.getAccounts()
+        .then(fetchedAccounts => {
+            console.log(fetchedAccounts);
+        })
+
+    // Use one of those accounts to deploy
+    // the contract
+
+});
+
+describe('Inbox', () => {
+    it('should deploy a contract', () => {});
+});
