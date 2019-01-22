3b40172a7ef577aacafa6dc16488515b6b175582
elzed <elzed@users.noreply.github.com>
2019-01-22 10:20:06 -0800

    Add basic deploy and send functionality and test

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -10,7 +10,11 @@ const Web3 = require('web3');
 // and tell it to connect to local test network
 const web3 = new Web3(ganache.provider());
 
+const { interface, bytecode } = require('../compile');
+
 let accounts;
+let inbox;
+
 beforeEach(async () => {
     // Get a list of all accounts
     // Use the eth module from web3
@@ -18,11 +22,23 @@ beforeEach(async () => {
 
     // Use one of those accounts to deploy
     // the contract
-
+    // ultimately, inbox is assigned an object on the blockchain
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({ data: bytecode, arguments: ['Hi there!'] })
+        .send({ from: accounts[0], gas: '1000000' })
 });
 
 describe('Inbox', () => {
     it('should deploy a contract', () => {
-        console.log(accounts);
+        assert.ok(inbox.options.address);
+    });
+
+    it('should have a default message', async () => {
+        const message = await inbox.methods.message().call();
+        assert.strictEqual(message, 'Hi there!');
+    });
+
+    it('should change the message', async () => {
+        
     });
 });
